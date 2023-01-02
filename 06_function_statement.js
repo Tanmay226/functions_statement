c4076d02c9e435941e97784feaa0146b900426d2
Tanmay Mandal <115643345+Tanmay226@users.noreply.github.com>
2023-01-02 22:56:08 +0530

    Add files via upload

---
+++ 06_function_statement.js
@@ -0,0 +1,37 @@
+//example...1
+
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(100,200);
+
+//example---2
+
+// function add(a,b){
+//     return(a+b);
+// }
+// let res=add(10,20);
+// console.log(res);
+
+//example --3
+// function greetings(){
+//     console.log("Hey Good Morning");
+// }
+// greetings();
+
+//Example --4
+function greeting(){
+    return("hey good morning");
+
+}
+console.log(greeting());
+
+//average function
+
+function avg(a,b){
+    return(a+b)/2;
+}
+c1= avg(8,16);
+c2 =  avg(2,4);
+console.log(c1,c2);//12 3
